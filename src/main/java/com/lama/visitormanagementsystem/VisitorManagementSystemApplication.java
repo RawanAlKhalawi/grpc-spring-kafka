5089391f75c3c5d752fbada256a823b4804b3651
Rawan AlKhalawi <rawanalkhalawi@gmail.com>
2021-03-17 22:42:22 +0300

    Initial commit

---
+++ src/main/java/com/lama/visitormanagementsystem/VisitorManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lama.visitormanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lama/visitormanagementsystem/VisitorManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lama.visitormanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
