cab72e8933565646b3e7e3ab8f30124388610c55
Rawan AlKhalawi <rawanalkhalawi@gmail.com>
2021-03-20 01:46:05 +0300

    Create Cassandra Database
    Closes #5

--- src/main/java/com/lama/visitormanagementsystem/config/CassandraConfig.java
+++ src/main/java/com/lama/visitormanagementsystem/config/CassandraConfig.java
@@ -35,7 +35,6 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
         return keySpace;
     }
 
-
     @Override
     protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
         return Collections.singletonList(CreateKeyspaceSpecification
