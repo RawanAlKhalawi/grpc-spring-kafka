43f91069f42fa748d3ead7c405cd0cbe8bc05aa0
Rawan AlKhalawi <rawanalkhalawi@gmail.com>
2021-04-10 18:09:27 +0000

    fix application image with cassandra
    Closes #42

--- src/main/java/com/lama/visitormanagementsystem/VisitorManagementSystemApplication.java
+++ src/main/java/com/lama/visitormanagementsystem/VisitorManagementSystemApplication.java
@@ -2,9 +2,8 @@ package com.lama.visitormanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class VisitorManagementSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lama/visitormanagementsystem/repository/VisitorRepository.java
+++ src/main/java/com/lama/visitormanagementsystem/repository/VisitorRepository.java
@@ -2,8 +2,10 @@ package com.lama.visitormanagementsystem.repository;
 
 import com.lama.visitormanagementsystem.model.Visitor;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface VisitorRepository extends CassandraRepository<Visitor, UUID> {
 }
