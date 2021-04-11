4961893e2184d45109f2c94ca56dd3d0e193899b
Rawan AlKhalawi <rawanalkhalawi@gmail.com>
2021-04-11 21:29:02 +0000

    Resolve "fix test job in gitlab ci"

--- src/test/java/com/lama/visitormanagementsystem/client/VisitorClient.java
+++ src/test/java/com/lama/visitormanagementsystem/client/VisitorClient.java
@@ -6,9 +6,7 @@ import com.lama.visitormanagementsystem.VisitorManagementSystemApplication;
 import com.lama.visitormanagementsystem.VisitorRequest;
 import com.lama.visitormanagementsystem.VisitorServiceGrpc;
 import io.grpc.ManagedChannel;
-import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
-import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
-import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
+import io.grpc.ManagedChannelBuilder;
 import org.grpcmock.springboot.AutoConfigureGrpcMock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import javax.net.ssl.SSLException;
-import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,14 +29,15 @@ public class VisitorClient {
     public void setup() throws SSLException {
         ManagedChannel channel;
 
-        File certFile = new File("src/main/resources/server.crt");
+//        File certFile = new File("src/main/resources/server.crt");
+//
+//        SslContext sslContext = GrpcSslContexts.forClient()
+//                .trustManager(certFile)
+//                .build();
 
-        SslContext sslContext = GrpcSslContexts.forClient()
-                .trustManager(certFile)
-                .build();
-
-        channel = NettyChannelBuilder.forAddress("localhost", 9090)
-                .sslContext(sslContext)
+        channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+                .usePlaintext()
+//                .sslContext(sslContext)
                 .build();
 
         this.visitorServiceBlockingStub = VisitorServiceGrpc.newBlockingStub(channel);
